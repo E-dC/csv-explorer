1dbd2a9df6e05f6eb411c0208537201803305fab
EdC <22207434+E-dC@users.noreply.github.com>
2020-07-09 17:10:23 +0100

    Improve the way the UI inputs are updated when loading a CSV.

--- R/utils.R
+++ R/utils.R
@@ -3,6 +3,8 @@ type2colnames <- function(df, x, col_names = names(df)){
                         'geo' = c('double', 'numeric'),
                         'lat' = c('double', 'numeric'),
                         'lon' = c('double', 'numeric'),
+                        'int' = c('integer'),
+                        'dbl' = c('double'),
                         'numeric' = c('numeric', 'integer', 'double'),
                         'char' = c('character'),
                         'fct' = c('factor'),
@@ -31,45 +33,69 @@ type2colnames <- function(df, x, col_names = names(df)){
   o[!is.na(o)]
 }
 
+
+#' @title Get \code{tibble}-guessed data types for each dataframe column
+#' 
+#' @description Get all of \code{df} columns' data types and put them in
+#'   a list usable to update the UI 
+#' @param df Dataframe created from the loaded CSV file.
+#' @return A list where each name is the \code{inputId} of an input in the
+#'   UI, and each value is a vector of column names with the correct
+#'   characteristics.
+#'   @import magrittr
+#'   @import dplyr
+get_df_coltypes <- function(df){
+  lat_cols <- df %>%
+    select(matches(lat_pat, ignore.case = TRUE) & where(is.double)) %>%
+    names()
+  lon_cols <- df %>%
+    select(matches(lon_pat, ignore.case = TRUE) & where(is.double)) %>%
+    names()
+
+  list('lat_cols' = lat_cols,
+       'lon_cols' = lon_cols,
+       'lgl_cols' = df %>%
+         select(where(is.logical)) %>%
+         names(),
+       'int_cols' = df %>%
+         select(where(is.integer)) %>%
+         names(),
+       'dbl_cols' = df %>%
+         select(-lat_cols & -lon_cols & where(is.double)) %>%
+         names(),
+       'char_cols' = df %>%
+         select(where(is.character)) %>%
+         names(),
+       'fct_cols' = df %>%
+         select(where(is.factor)) %>%
+         names(),
+       'date_cols' = df %>%
+         select(where(lubridate::is.Date)) %>%
+         names()
+         )
+}
+
+#' @title Update type controls in the UI
+#' 
+#' @description  When a new CSV file is loaded, update the select list input controls
+#'   on the starting UI tab
+#'   
+#' @param df Dataframe created from the loaded CSV file.
+#' @param session The session object passed to function given to shinyServer.
+#' @return Nothing, we only want a side effect here 
 update_type_inputs <- function(df, session){
   choices <- names(df)
+  col_types <- get_df_coltypes(df)
   
-  lat <- type2colnames(df, 'lat')
-  
-  updateSelectInput(session,
-                    'lat_cols',
-                    choices = choices,
-                    selected = lat)
-  
-  lon <- type2colnames(df, 'lon')
-  # print(lon)
-  updateSelectInput(session,
-                    'lon_cols',
-                    choices = choices,
-                    selected = lon)
-  
-  col_names <- choices[! choices %in% c(lat, lon)]
-  
-  updateSelectInput(session,
-                    'real_cols',
-                    choices = choices,
-                    selected = type2colnames(df, 'numeric', col_names))
-  
-  updateSelectInput(session,
-                    'char_cols',
-                    choices = choices,
-                    selected = type2colnames(df, 'char', col_names))
-  
-  updateSelectInput(session,
-                    'factor_cols',
-                    choices = choices,
-                    selected = type2colnames(df, 'fct', col_names))
-  
-  updateSelectInput(session,
-                    'lgl_cols',
-                    choices = choices,
-                    selected = type2colnames(df, 'lgl', col_names))
+  mapply(function(input_ref, selected) {
+    updateSelectInput(session = session,
+                      inputId = input_ref,
+                      choices = choices,
+                      selected = selected)},
+    input_ref = names(col_types),
+    selected = col_types)
 }
+
 
 #' @param text Character
 #' @param search Character. Will be passed to stringr::regex, with ignore_case = TRUE
